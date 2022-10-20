8b09b086b759c710ac1bea006ce8ce82e1bac446
Gage Hall <Gageh310@gmail.com>
2022-10-20 11:11:16 -0400

    LoopsExercise Completed

---
+++ script.js
@@ -0,0 +1,26 @@
+for (i = 1; i <= 7; i++){
+    console.log(i);
+}
+
+
+for (i = 5; i <= 25; i += 4){
+    console.log(i)
+}
+
+const wizards = [
+    `Harry Potter`,
+    `Hermione Granger`,
+    `Ron Weasley`
+]
+
+for (wiz of wizards){
+    console.log(wiz);
+}
+
+let harryPotterMovies = 0;
+
+while (harryPotterMovies != 8){
+    harryPotterMovies++;
+    console.log(harryPotterMovies);
+}
+
